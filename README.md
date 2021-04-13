8b85757d40906f4bcfa1329c1f5b16adfdcf2104
Tibinko <tibor.hanesz@gmail.com>
2021-04-13 08:57:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## TODO
+
+- unicorn/filename-case
+- unicorn/no-null
+- unicorn/no-useless-undefined
+- unicorn/no-array-callback-reference
---
+++ index.js
@@ -0,0 +1,53 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es6: true,
+    node: false,
+    browser: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:eslint-comments/recommended",
+    "plugin:promise/recommended",
+    "plugin:unicorn/recommended",
+    "xo",
+    "xo/esnext",
+    "xo/browser",
+    "react-app",
+    "plugin:prettier/recommended",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2018,
+  },
+  plugins: ["react-hooks", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-redeclare": ["error"],
+    "@typescript-eslint/prefer-optional-chain": "warn",
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+    "no-async-promise-executor": "error",
+    "no-redeclare": "off",
+    "prettier/prettier": ["error"],
+    "react-hooks/rules-of-hooks": "error",
+    "react/self-closing-comp": [
+      "error",
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    "no-warning-comments": "off",
+    "unicorn/filename-case": "off",
+    "unicorn/prevent-abbreviations": [
+      "error",
+      {
+        ignore: ["param", "Params", "err", "props", "i18n"],
+      },
+    ],
+    "unicorn/no-null": "off",
+    "unicorn/consistent-function-scoping": "off",
+    "unicorn/no-useless-undefined": "off",
+  },
+};
